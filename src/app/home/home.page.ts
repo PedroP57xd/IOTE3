eeb6c91b183d4284f6c7095bd7437f379dd2bb3e
PEDRO <130096211+PedroP57xd@users.noreply.github.com>
2024-02-07 15:04:12 -0500

    a

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,17 +10,11 @@ export class HomePage {
 
 rutadetabla:any;
 ledStatus: boolean = false;
+ledStatus2: boolean = false;
+ledStatus3: boolean = false;
+ledStatus4: boolean = false;
 constructor(private db:Firestore) {
 
-}
-async apagar() {
-  this.rutadetabla = doc(this.db,'controlLed','LED1');//RUTA DE TABLA EN LA BD
-  await setDoc(this.rutadetabla, { encender: false });//CAMBIA EL ATRIBUTO DE LA TABLA
-}
-
-async encender() {
-  this.rutadetabla = doc(this.db,'controlLed','LED1');//RUTA DE TABLA EN LA BD
-  await setDoc(this.rutadetabla, { encender: true });//CAMBIA EL ATRIBUTO DE LA TABLA
 }
 
 async toogleState() {
@@ -29,4 +23,35 @@ async toogleState() {
   await setDoc(this.rutadetabla, { encender:this.ledStatus});//CAMBIA EL ATRIBUTO DE LA TABLA
   console.log(this.ledStatus);
 }
+async toogleState2() {
+  this.ledStatus2=! this.ledStatus2;
+  this.rutadetabla = doc(this.db,'controlLed','LED2');//RUTA DE TABLA EN LA BD
+  await setDoc(this.rutadetabla, { encender:this.ledStatus2});//CAMBIA EL ATRIBUTO DE LA TABLA
+  console.log(this.ledStatus2);
+}
+async toogleState3() {
+  this.ledStatus3=! this.ledStatus3;
+  this.rutadetabla = doc(this.db,'controlLed','LED3');//RUTA DE TABLA EN LA BD
+  await setDoc(this.rutadetabla, { encender:this.ledStatus3});//CAMBIA EL ATRIBUTO DE LA TABLA
+  console.log(this.ledStatus3);
+}
+async toogleState4() {
+  this.ledStatus=! this.ledStatus4;
+  this.ledStatus2=! this.ledStatus4;
+  this.ledStatus3=! this.ledStatus4;
+  this.ledStatus4=! this.ledStatus4;
+  this.rutadetabla = doc(this.db,'controlLed','LED1');//RUTA DE TABLA EN LA BD
+  this.rutadetabla = doc(this.db,'controlLed','LED2');//RUTA DE TABLA EN LA BD
+  this.rutadetabla = doc(this.db,'controlLed','LED3');//RUTA DE TABLA EN LA BD
+  this.rutadetabla = doc(this.db,'controlLed','LEDS');//RUTA DE TABLA EN LA BD
+  await setDoc(this.rutadetabla, { encender:this.ledStatus4});//CAMBIA EL ATRIBUTO DE LA TABLA
+  await setDoc(this.rutadetabla, { encender:this.ledStatus4});//CAMBIA EL ATRIBUTO DE LA TABLA
+  await setDoc(this.rutadetabla, { encender:this.ledStatus4});//CAMBIA EL ATRIBUTO DE LA TABLA
+  await setDoc(this.rutadetabla, { encender:this.ledStatus4});//CAMBIA EL ATRIBUTO DE LA TABLA
+  console.log(this.ledStatus);
+  console.log(this.ledStatus2);
+  console.log(this.ledStatus3);
+  console.log(this.ledStatus4);
+}
+
 }
